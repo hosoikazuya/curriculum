b03685829b59c039d9b74b2330a05408d9acb276
hosoikazuya <hosoikazuya@gmail.com>
2021-04-13 16:55:20 +0900

    課題提出
    Task1_1.java
    Task1_1.class

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,19 @@
+public class Task1_1{
+  public static void main(String[] args){
+
+    int hogeInt=50;
+    String hogeString="テスト";
+    
+    //double型のhogeDoubleという名前の変数を宣言し値[0.2]で初期化
+    double hogeDouble=0.2;
+
+    hogeInt=100;
+    hogeString="合格";
+
+    System.out.println(hogeInt);
+    System.out.println(hogeString);
+    //変数 hogeStringの値を表示
+    System.out.println(hogeDouble);
+    
+  }
+}
