83ee97b15733f43abc1a09c395af5ffff5ebaf56
hosoikazuya <hosoikazuya@gmail.com>
2021-04-13 22:59:28 +0900

    課題提出
    Task1_2.java
    Task1_2.class

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,16 @@
+public class Task1_2{
+    public static void main(String[] args){
+
+        String[] country={"JAPAN","AMERICA","KORIA"};
+        System.out.println(country.length);
+
+        String[] strArray={"りんご","もも","ぶどう"};
+        System .out.println(strArray[1]);
+
+        //「１０」「２０」「３０」「４０」「５０」を要素の値とするintArrayの作成
+        int[] intArray={10,20,30,40,50};
+        //要素「１」と「４」を足した出力
+        System.out.println(intArray[1]+intArray[4]);
+
+    }
+}
