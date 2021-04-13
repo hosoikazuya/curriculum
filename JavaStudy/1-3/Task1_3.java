790fe5c8e2dc5d43491abe5c516262f118ccf320
hosoikazuya <hosoikazuya@gmail.com>
2021-04-14 00:13:28 +0900

    課題提出
    Task1_3.java
    Task1_3.class

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,34 @@
+public class Task1_3{
+    public static void main(String[] args){
+
+        int plus=5+10;
+        
+        int minus=20-7;
+        
+        //10と2を乗算した値を代入する変数 multiplyの作成
+        int multiply=10*2;
+
+        int rimi=20%6;
+
+        //有楽町を代入した変数townと線を代入した変数lineの作成
+        String town="有楽町";
+        String line="線";
+
+        String train=town+line;
+
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(rimi);
+        System.out.println(train);
+        
+    
+
+
+
+    
+
+
+
+    }
+}
