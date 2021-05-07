4751f3126dff2a10c3176dfed72247758b2387e4
hosoikazuya <hosoikazuya@gmail.com>
2021-05-08 03:18:46 +0900

    課題提出Check2

---
+++ JavaStudy/Chack2/src/check/Check.java
@@ -0,0 +1,32 @@
+package check;
+
+import constants.Constants;
+
+public class Check extends Constants{
+
+	private static String firstName = "細井";
+	private static String lastName = "一哉";
+	
+	private static String printName (String firstName, String lastName) {
+		return firstName + lastName;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println(
+				"printNameメソッド →　" + printName(firstName, lastName));
+	
+		
+		
+		Pet pet = new Pet(CHECK_CLASS_JAVA,CHECK_CLASS_HOGE);
+		pet.introduce();
+		
+		RobotPet robotPet = new RobotPet(CHECK_CLASS_R2D2, CHECK_CLASS_R2D2);
+		robotPet.introduce();	
+	}
+
+	
+
+}
