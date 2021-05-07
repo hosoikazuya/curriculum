9c635d2866a545a3ef5046e9264b6830b36e72b4
hosoikazuya <hosoikazuya@gmail.com>
2021-05-08 03:17:52 +0900

    課題再提出2-6

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -27,7 +27,7 @@ public class Study {
 
         // 問② 「(Calendar.MONTH) + 数値」の処理は、想定していない値となる可能性があります。
         // その理由をコメントへ記述してください。
-        // [ここへ記述]うるう年が反映されていないため、うるう年の場合想定していない値になる可能性がある。
+        // [ここへ記述]getメソッドを使用した場合。あくまで＋数値になるため１３月などの表記になる場合がある。
         System.out.println(calendar.get(Calendar.YEAR));
         System.out.println(calendar.get(Calendar.MONTH) + 1);
         System.out.println(calendar.get(Calendar.DATE));
@@ -35,10 +35,10 @@ public class Study {
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
         calendar.add(Calendar.YEAR, 1);
-        calendar.add(Calendar.MONTH, 2);
+        calendar.add(Calendar.MONTH, 3);
         calendar.add(Calendar.DATE , 15);
         System.out.println(calendar.get(Calendar.YEAR));
-        System.out.println(calendar.get(Calendar.MONTH) + 1);
+        System.out.println(calendar.get(Calendar.MONTH));
         System.out.println(calendar.get(Calendar.DATE));
         
         // うるう年判定
